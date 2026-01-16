a0dce35b4a9cbbda98df1a9ac75cd4f4a1fba06a
shusaini-tech <shusaini@cddwestafrica.org>
2026-01-16 15:05:31 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,30 +8,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, fontFamily: "system-ui, sans-serif" }}>
-        {/* Language Toggle */}
-        <div
-          style={{
-            padding: "0.75rem 8vw",
-            fontSize: "0.9rem",
-            background: "#f6f2ec",
-            display: "flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <span style={{ marginRight: "0.5rem", fontWeight: 600 }}>EN</span>
-          <span style={{ margin: "0 0.25rem" }}>|</span>
-          <a
-            href="/fr"
-            style={{
-              textDecoration: "none",
-              color: "#4a3324",
-              fontWeight: 600,
-            }}
-          >
-            FR
-          </a>
-        </div>
-
         {children}
       </body>
     </html>
